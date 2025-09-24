ded53f46750831b5e41693b093879a2f0163aace
sn0r1ax <2561946002@qq.com>
2025-09-24 13:32:26 +0800

    左右移动逻辑有问题

--- include/board.h
+++ include/board.h
@@ -10,10 +10,11 @@ class Board
     public:
     const int BOARD_WIDTH = 10;
     const int BOARD_HEIGHT = 15;
-    static int board[10][15];//棋盘矩阵,0,1,2对应三种状态
+    static int board[12][16];//棋盘矩阵,0,1,2对应三种状态
     //0代表无方块
     //1代表运动的方块
     //2代表不动的方块
+    //3代表边界
     void init();
 };
 #endif //BOARD_H
--- src/game.cpp
+++ src/game.cpp
@@ -26,13 +26,13 @@ void Game::init()
 
 void Game::blockGenerate(Graph &g)
 {
-    for (int i = 0;i < 3;i++)
+    for (int y = 0;y < 3;y++)
     {
-        for (int j = 0;j < 3;j++)
+        for (int x = 0;x < 3;x++)
         {
-            if (g.matrix[j][i] == 1)
+            if (g.matrix[x][y] == 1)
             {
-                board[j+4][i] = 1;
+                board[x+4][y] = 1;
             }
         }
     }
@@ -52,12 +52,12 @@ void Game::blockGenerate(Graph &g)
 
 void Game::drawBoard()
 {
-    for (int i = 0;i < 15;i++)//y
+    for (int y = 0;y < 15;y++)//y
     {
-        for (int j = 0;j < 10;j++)//x
+        for (int x = 0;x < 10;x++)//x
         {
-            if (board[j][i])
-                mvaddch(i,j,'#');
+            if (board[x][y])
+                mvaddch(y,x,'#');
         }
     }
 }
@@ -78,12 +78,12 @@ void Game::eraseBlock(Graph &g,int x, int y)
 
 void Game::eraseBoard()
 {
-    for (int i = 0;i < 15;i++)
+    for (int y = 0;y < 15;y++)
     {
-        for (int j = 0;j < 10;j++)
+        for (int x = 0;x < 10;x++)
         {
-            if (board[j][i] == 1)
-                mvaddch(i,j,' ');
+            if (board[x][y] == 1)
+                mvaddch(y,x,' ');
         }
     }
     // refresh();
@@ -95,12 +95,38 @@ void Game::processInput()//键盘操作逻辑
     if(ch == 260)
     {
         if (!game.isBorderL())//未出界 靠近左边或右边一列的数据不能为1
-            x--;
+            //左移逻辑
+        {
+            for (int y = 0;y < 15;y++)
+            {
+                for (int x = 0;x < 10;x++)
+                {
+                    if (board[x][y] == 1)
+                    {
+                        board[x][y] = 0;
+                        board[x-1][y] = 1;
+                    }
+                }
+            }
+        }
     }
     else if(ch == 261)//右
     {
         if (!game.isBorderR())//未出界
-            x++;
+            //右移逻辑
+        {
+            for (int y = 0;y < 15;y++)
+            {
+                for (int x = 0;x < 10;x++)
+                {
+                    if (board[x][y] == 1)
+                    {
+                        board[x][y] = 0;
+                        board[x+1][y] = 1;
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -114,27 +140,29 @@ void Game::update()//方块下落逻辑
      */
     if (isBottom())//触底了
     {//1全变2
-        for (int i = 0;i < 15;i++)
+        for (int y = 0;y < 15;y++)
         {
-            for (int j = 0;j < 10;j++)
+            for (int x = 0;x < 10;x++)
             {
-                if (board[i][j] == 1)
-                    board[i][j] = 2;
+                if (board[x][y] == 1)
+                    board[x][y] = 2;
             }
         }
+        Square square;
+        blockGenerate(square);
     }
     else
     {
         game.eraseBoard();
         //为1的块集体向下移一格
-        for (int i = 14;i >= 0;i--)
+        for (int y = 14;y >= 0;y--)
         {
-            for (int j = 9;j >= 0;j--)
+            for (int x = 9;x >= 0;x--)
             {
-                if (board[j][i] == 1)
+                if (board[x][y] == 1)
                 {
-                    board[j][i] = 0;
-                    board[j][i+1] = 1;
+                    board[x][y] = 0;
+                    board[x][y+1] = 1;
                 }
             }
         }
@@ -157,13 +185,13 @@ void Game::render()//方块刷新逻辑 只有在触底时刷新方块
 //底部/堆叠判断逻辑
 bool Game::isBottom()
 {
-    for (int i = 0;i < 15;i++)
+    for (int y = 0;y < 15;y++)
     {
-        for (int j = 0;j < 10;j++)
+        for (int x = 0;x < 10;x++)
         {
-            if (board[j][i] == 1)//找到正在运动的块
-            {
-                if (board[j][i+1] == 2||board[j][i+1] == '-')//正在运动的块的下面是不动的块或边界
+            if (board[x][y] == 1)//找到正在运动的块
+            {//3代表边界
+                if (board[x][y+1] == 2||y == 14)//正在运动的块的下面是不动的块或边界
                     return true;
             }
         }
@@ -174,19 +202,19 @@ bool Game::isBottom()
 //边界判断逻辑
 bool Game::isBorderL()
 {
-    for (int i = 0;i < 15;i++)
+    for (int y = 0;y < 15;y++)
     {
-        if (board[1][i] == 1)//判断方块位置上到边缘
+        if (board[1][y] == 1)//判断方块位置上到边缘
             return true;
     }
     //还需考虑旁边是静止块
-    for (int i = 0;i < 15;i++)
+    for (int y = 0;y < 15;y++)
     {
-        for (int j = 0;j < 10;j++)
+        for (int x = 0;x < 10;x++)
         {
-            if (board[j][i] == 1)
+            if (board[x][y] == 1)
             {
-                if (board[j][i-1] == 2)
+                if (board[x][y-1] == 2)
                     return true;
             }
         }
@@ -195,19 +223,19 @@ bool Game::isBorderL()
 }
 bool Game::isBorderR()
 {
-    for (int i = 0;i < 15;i++)
+    for (int y = 0;y < 15;y++)
     {
-        if (board[10][i] == 1)//判断方块位置上到边缘
+        if (board[10][y] == 1)//判断方块位置上到边缘
             return true;
     }
     //还需考虑旁边是静止块
-    for (int i = 0;i < 15;i++)
+    for (int y = 0;y < 15;y++)
     {
-        for (int j = 0;j < 10;j++)
+        for (int x = 0;x < 10;x++)
         {
-            if (board[j][i] == 1)
+            if (board[x][y] == 1)
             {
-                if (board[j][i+1] == 2)
+                if (board[x][y+1] == 2)
                     return true;
             }
         }
